4738a1b6741b2b3cd7fd8d711e134dead3795650
bodvarsdottirkristin <bodvarsdottirkristin@gmail.com>
2025-10-29 16:37:17 +0100

    Initial clean commit (no venv, add .gitignore)

---
+++ lib/losses.py
@@ -0,0 +1,34 @@
+import torch
+from torch import nn
+
+class BCELoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, y_pred, y_true):
+        loss = torch.mean(y_pred - y_true*y_pred + torch.log(1 + torch.exp(-y_pred)))
+        return loss
+
+class DiceLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, y_pred, y_true):
+        raise Exception("Implement this!")
+
+class FocalLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, y_pred, y_true):
+        raise Exception("Implement this!")
+
+class BCELoss_TotalVariation(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, y_pred, y_true):
+        loss = torch.mean(y_pred - y_true*y_pred + torch.log(1 + torch.exp(-y_pred)))
+        regularization = ...
+        return loss + 0.1*regularization
+
